f92f7c2f7e92a6b5f100181d43d5ec5b53a03017
Jojo Thomas <jojothomas1515@gmail.com>
2023-10-08 16:35:58 +0100

    done

--- config/db.js
+++ config/db.js
@@ -3,10 +3,10 @@ const { Sequelize } = require("sequelize");
 console.log(process.env.DATABASE_URL);
 const sequelize = new Sequelize({
   dialect: "postgres",
-  host: "localhost",
-  username: "hng",
-  password: "hngninja",
-  database: "hng_auth",
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PWD,
+  database: process.env.DB_NAME,
 });
 
 module.exports = sequelize;
