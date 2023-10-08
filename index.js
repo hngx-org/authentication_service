0c45ac5150e8a32960b8ece2c666babc6ae662d6
hardeymolar <abdulmalikgbolahan95@gmail.com>
2023-10-08 09:12:39 +0100

    swagger ui initialized

--- index.js
+++ index.js
@@ -2,14 +2,22 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require('./swaggerUi');
+const sampleRoutes = require("./routes/demo");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Serve Swagger UI
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// Use your API routes
+app.use('/api', sampleRoutes);
+
 // Routes
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
