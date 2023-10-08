e0402f228901c4894793980f22ef70e5851b70d1
holabayor <liasu.olabayo@gmail.com>
2023-10-08 16:42:19 +0100

    update auth routes

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/auth', authRoutes);
 app.use(bodyParser.json());
 
 // Sync the model with the database
@@ -25,9 +24,9 @@ app.use(passport.initialize());
 require('./middleware/authEmail')(passport);
 
 // Routes
+app.use('/api/auth', authRoutes);
 app.use('/auth', userRoute);
 // require('./routes/userRoutes')(app);
-app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
