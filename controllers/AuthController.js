6962ab2cc197b465568be9d12b490ae816c0a568
Samuel Ademujimi <samshotmedia01@gmail.com>
2023-10-08 13:51:12 +0100

    set up functions for 2fa

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const Joi = require('joi');
+const User = require("../models/User");
 
 const secretKey = 'your-secret-key'; // Replace with your own secret key
 
@@ -49,8 +50,69 @@ const enable2fa = (req, res) => {
     return res.status(400).json({ message: error.details[0].message });
   }
 
-  // ... The middleware for sending mails is not ready.
+  
+
+ const user = await User.findOne( 
+       { where: { email: data.email } 
+       } 
+  );
+
+if(!user) return res.status(400).json({ message: "User not found" });
+ 
+
+user.two_factor_enabled = true;
+user.save();
   res.status(200).json({ message: '2fa enabled successfully ' });
 };
 
-module.exports = { forgotPassword, resetPassword, enable2fa };
+
+
+const send2faCode = (req, res) => {
+  const { error } = enable2faSchema.validate(req.body);
+
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+
+  
+
+ const user = await User.findOne( 
+       { where: { email: data.email } 
+       } 
+  );
+
+if(!user) return res.status(400).json({ message: "User not found" });
+ 
+
+// The middleware for sending code is not ready
+
+
+
+
+  res.status(200).json({ message: 'You have been sent a code ' });
+};
+
+
+const verify2fa = (req, res) => {
+  const { error } = verify2faSchema.validate(req.body);
+
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+
+  
+
+ const user = await User.findOne( 
+       { where: { email: data.email } 
+       } 
+  );
+
+if(!user) return res.status(400).json({ message: "User not found" });
+ 
+const { token } = req.body;
+user.two_factor_enabled = true;
+user.save();
+  res.status(200).json({ message: '2fa enabled successfully ' });
+};
+
+module.exports = { forgotPassword, resetPassword, enable2fa , send2faCode, verify2fa};
