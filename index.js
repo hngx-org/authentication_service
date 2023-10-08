d9ee84ce268f1b00dab0c48c64f5b1ba9af51389
marrwan <abdulbasitdamilola6@gmail.com>
2023-10-08 13:01:24 +0100

    added configuration for passport and routes

--- index.js
+++ index.js
@@ -2,14 +2,24 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const session = require("express-session");
+const passport = require("passport");
+
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(session({ secret: 'HNGx',resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+require('./config/githubStrategy');
 
 // Routes
+const authRoutes = require('./routes/auth');
+
+app.use("/auth", authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
