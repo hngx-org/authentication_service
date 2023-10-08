1bb2e460a9f7826149e4cd6a4e543f338b7cf16f
Olawole Olawale Martins <olawalemart@gmail.com>
2023-10-08 18:58:20 +0100

    ensure getAuth end point

--- routes/getAuth.js
+++ routes/getAuth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { getAuth, getAuthPermissions } = require('../controllers/getAuth');
 const router = express.Router();
 
-router.get('/', getAuth);
-router.get('/all-permissions', getAuthPermissions);
+router.post('/', getAuth);
+router.post('/all-permissions', getAuthPermissions);
 
 module.exports = router;
