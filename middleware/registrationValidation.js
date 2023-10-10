90be0532d3b0004521413b2fed22f02bbe991b81
Akuya Ekorot <akuyaekorot@gmail.com>
2023-10-10 08:06:27 +0300

    fix user registration validation middleware

--- middleware/registrationValidation.js
+++ middleware/registrationValidation.js
@@ -4,48 +4,45 @@ const User = db.user;
 
 const registrationValidation = (req, res, next) => {
   try {
-    const { username, email, password } = req.body;
+    const { firstName, lastName, email, password } = req.body;
 
-    check(username, "Username is required").notEmpty();
-    check(email, "Email is required")
+    check(firstName)
+      .notEmpty()
+      .withMessage("First name is required")
+      .isString()
+      .withMessage("First name must be a string");
+
+    check(lastName)
+      .notEmpty()
+      .withMessage("Last name is required")
+      .isString()
+      .withMessage("Last name must be a string");
+
+    check(email)
+      .notEmpty()
+      .withMessage("Email is required")
       .isEmail()
+      .withMessage("Email is invalid");
+
+    check(password)
       .notEmpty()
-      .withMessage("invalid email address");
-    check(password, "Password is required")
-      .notEmpty()
-      .isLength({ min: 8 })
-      .withMessage("Password must be at least 6 characters long") // checks if password is atleast must be at least 6 chars long
-      .matches(/[0-9]/).withMessage('Password must contain at least one number')
-      .matches(/[!@#$%^&*]/).withMessage('Password must contain at least one special character');
-
-    email.custom(async (value) => {
-      const user = await User.findOne({ where: { email: value } });
-      if (user) {
-        throw new Error("E-mail already in use");
-      }
-    });
-
-    username.custom(async (value) => {
-      const user = await User.findOne({ where: { username: value }});
-      if (user) {
-        throw new Error("Username already in use");
-      }
-    });
-
-    passwordConfirmation.custom((value, { req }) => {
-        if (value !== req.body.password) {
-            throw new Error("Password confirmation does not match password");
-        }
-        return value === req.body.password;
-    });
+      .withMessage("Password is required")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters long")
+      .matches(/\d/)
+      .withMessage("Password must contain a number")
+      .matches(/[!@#$%^&*(),.?":{}|<>]/)
+      .withMessage("Password must contain a special character");
 
     const errors = validationResult(req);
+
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() });
     }
+
     next();
   } catch (err) {
-    res.status(500).send({ message: err.message })
+    res.status(500).send({ message: err.message });
   }
 };
 
