16bdec780b65edf8b2a69e05cdc79acd7db5b134
Ireoluwatobi <111185454+Ireoluwatobi@users.noreply.github.com>
2023-10-10 14:44:34 +0100

    fixed github login errors

--- controllers/githubLoginController.js
+++ controllers/githubLoginController.js
@@ -1,38 +1,54 @@
 const { getGithubUser } = require("../helpers/getGitUser");
-const User = require("../models/User");
+const User = require("../models/Users");
 const jwt = require("jsonwebtoken");
+const clientId = process.env.GITHUB_LCLIENT_ID;
+const redirectUrl = process.env.GITHUB_REDIRECT_URL
+const githubLogin = async (req, res) => {
+try {
+  return res.redirect(`https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUrl}?path=/&scope=user:email`)
+} catch (error) {
+  throw new Error(error)
+}
+};
 
-const githubLogin = async (req, res, next) => {
+const githubRedirectUrl = async(req,res)=>{
   try {
     const { code, path } = req.query;
     if (!code) {
       throw new Error("no code or in query");
     }
-    const githubUser = await getGithubUser(code);
-    const { email } = githubUser;
+    let githubUser = await getGithubUser(code)
+    githubUser = githubUser.filter((user)=>{
+      return user.primary == true
+      });
+    console.log(githubUser)
+    const { email } = githubUser[0];
     if (!email) {
-      res.status(403).json({
+     return res.status(403).json({
         status: "fail",
         message: "No Email from github",
       });
     }
     const user = await User.findOne({ where: { email } });
     if (!user) {
-      res.status(400).json({
+      return res.status(400).json({
         status: "fail",
         message: "No User with github Account Found.",
       });
     }
     const secret = process.env.JWT_SECRET;
     const payload = { id: user.id };
-    const token = jwt.sign(payload, secret);
+    const token = jwt.sign(payload, secret, {
+      expiresIn : '1d'
+    });
 
     return res.status(200).json({ token, data: user, statusCode: 200 });
   } catch (error) {
-    next(error)
+    throw new Error(error)
   }
-};
+}
 
 module.exports = {
   githubLogin,
+  githubRedirectUrl
 };
