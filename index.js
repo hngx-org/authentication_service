2af15d3313bbcf1ccbe5e9f91ddcff9a02fdf1e8
hardeymolar <abdulmalikgbolahan95@gmail.com>
2023-10-11 13:25:31 +0100

    error handler middleware initialized

--- index.js
+++ index.js
@@ -7,10 +7,24 @@ const passport = require('passport');
 const defineRolesandPermissions = require('./helpers/populate');
 const userAuthRoutes = require('./routes/auth');
 const getAuthRoutes = require('./routes/getAuth');
+const {
+  errorLogger,
+  errorHandler,
+} = require("./middleware/errorHandlerMiddleware");
+const { UNKNOWN_ENDPOINT } = require("./errors/httpErrorCodes");
+
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: true, // Enable preflight requests
+  optionsSuccessStatus: 204, // Use 204 No Content for preflight success status
+};
+
+app.options('*', cors(corsOptions)); // Set up a global OPTIONS handler
+app.use(cors(corsOptions)); // Use the configured CORS middleware for all routes
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -36,6 +50,19 @@ app.use('/api/auth', userAuthRoutes);
 
 //communication with other microservices
 app.use('/api/get-auth', getAuthRoutes);
+
+
+// Serving Files
+http: app.use(errorLogger);
+app.use(errorHandler);
+
+// app.use("/auth", auth);
+
+// 404 Route handler
+http: app.use((req, res) => {
+  // use custom helper function
+  res.error(404, "Resource not found", UNKNOWN_ENDPOINT);
+});
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
