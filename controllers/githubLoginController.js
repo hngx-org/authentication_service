ecf9118247a0848f49c4b3d6e5ba0a10253421bb
hardeymolar <abdulmalikgbolahan95@gmail.com>
2023-10-11 13:44:29 +0100

    errorhandler added to all controller files

--- controllers/githubLoginController.js
+++ controllers/githubLoginController.js
@@ -1,6 +1,25 @@
 const { getGithubUser } = require("../helpers/getGitUser");
 const User = require("../models/Users");
 const jwt = require("jsonwebtoken");
+const {
+  ResourceNotFound,
+  Unauthorized,
+  BadRequest,
+  Conflict,
+  Forbidden,
+  ServerError,
+} = require("../errors/httpErrors");
+const {
+  RESOURCE_NOT_FOUND,
+  ACCESS_DENIED,
+  INVALID_TOKEN,
+  MISSING_REQUIRED_FIELD,
+  INVALID_REQUEST_PARAMETERS,
+  EXISTING_USER_EMAIL,
+  EXPIRED_TOKEN,
+  CONFLICT_ERROR_CODE,
+  THIRD_PARTY_API_FAILURE,
+} = require("../errors/httpErrorCodes");
 const clientId = process.env.GITHUB_LCLIENT_ID;
 const redirectUrl = process.env.GITHUB_REDIRECT_URL
 const githubLogin = async (req, res) => {
