542f30c6f11a9d4d8c295a6a30d868624c2f1ef3
sapphire <chidinmaakunwa@gmail.com>
2023-10-12 10:43:40 +0100

    Update demo.js

--- routes/demo.js
+++ routes/demo.js
@@ -8,4 +8,24 @@ router.get('/sample', (req, res) => {
   res.json({ message: 'This is a sample response' });
 });
 
+router.get('/user', verifyJwt, checkRole([2]), (req, res) => {
+  res.json({
+    "status": 200,
+    "success": true,
+    message: 'This is a user sample response' });
+});
+
+router.get('/admin', verifyJwt, checkRole([3]), (req, res) => {
+  res.json({
+    "status": 200,
+    "success": true,
+    message: 'This is an admin sample response' });
+});
+router.get('/adminuser', verifyJwt, checkRole([2,3]), (req, res) => {
+  res.json({
+    "status": 200,
+    "success": true,
+    message: 'This is an adminuser sample response' });
+});
+
 module.exports = router;
