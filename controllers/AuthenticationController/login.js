08453894da4ba85918afb1ed8b7cb6ecad2c797b
Olawole Olawale Martins <olawalemart@gmail.com>
2023-10-14 17:02:18 +0100

    updated login route

--- controllers/AuthenticationController/login.js
+++ controllers/AuthenticationController/login.js
@@ -1,6 +1,5 @@
 const axios = require('axios');
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 const User = require('../../models/Users');
 
 const login = async (req, res, next) => {
@@ -16,15 +15,6 @@ const login = async (req, res, next) => {
   const isMatch = bcrypt.compareSync(password, user.password);
 
   if (!isMatch) return res.status(401).json({ message: 'Invalid password' });
-
-  req.user = user;
-  return next();
-  // const jwt_payload = {
-  //   id: user.id,
-  // };
-
-  // Check it user has 2fa enabled, then send 2fa code to user email
-
   if (user.two_factor_auth) {
     const code = Math.floor(100000 + Math.random() * 900000).toString();
 
@@ -37,25 +27,8 @@ const login = async (req, res, next) => {
     });
   }
 
-  // const token = jwt.sign(jwt_payload, process.env.JWT_SECRET);
-
-  // res.header('Authorization', `Bearer ${token}`);
-
-  // return res.status(200).json({
-  //   status: 200,
-  //   message: 'Login successful',
-  //   data: {
-  //     token,
-  //     user: {
-  //       id: user.id,
-  //       firstName: user.first_name,
-  //       lastName: user.last_name,
-  //       email: user.email,
-  //       is_verified: user.is_verified,
-  //       two_factor_auth: user.two_factor_auth,,
-  //     },
-  //   },
-  // });
+  req.user = user;
+  return next();
 };
 
 module.exports = login;
