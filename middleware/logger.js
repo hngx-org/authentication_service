ff63246f70ec1e6084373cb2c339007e7f100d23
TheShinobi <billmal071@gmail.com>
2023-10-15 14:20:17 +0100

    chore: get latest code

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,5 +1,25 @@
 const log4js = require('log4js');
 
+log4js.configure({
+  appenders: {
+    file: {
+      type: 'file',
+      filename: 'logs/debug.log',
+      maxLogSize: 10485760,
+      backups: 3,
+      compress: true,
+    },
+    console: {
+      type: 'console',
+    },
+  },
+  categories: {
+    default: {
+      appenders: ['file', 'console'],
+      level: 'debug',
+    },
+  },
+});
 const logger = log4js.getLogger();
 
 module.exports = logger;
