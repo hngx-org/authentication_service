9cdeb8558d8082c586d1c4223ec432f383133b52
Olawole Olawale Martins <olawalemart@gmail.com>
2023-10-15 17:41:07 +0100

    updated the response of send 2fa to enable proper front end routing

--- helpers/send2fa.js
+++ helpers/send2fa.js
@@ -3,34 +3,32 @@ const axios = require('axios');
 
 require('dotenv').config();
 
-
 const send2fa = async (user) => {
-    const EMAIL_SERVICE_2FA_URL = process.env.EMAIL_SERVICE_2FA_URL;
+  const EMAIL_SERVICE_2FA_URL = process.env.EMAIL_SERVICE_2FA_URL;
 
-    const code = Math.floor(100000 + Math.random() * 900000).toString();
+  const code = Math.floor(100000 + Math.random() * 900000).toString();
 
-    const responseObj = {}
+  const responseObj = { email: user.email, twoFactorAuth: true };
 
-    await user.update({ refresh_token: code });
+  await user.update({ refresh_token: code });
 
-    try {
-        const response = await axios.post(EMAIL_SERVICE_2FA_URL, {
-            recipient: user.email,
-            name: user.first_name,
-            code,
-        });
+  try {
+    const response = await axios.post(EMAIL_SERVICE_2FA_URL, {
+      recipient: user.email,
+      name: user.first_name,
+      code,
+    });
 
-        if (response.status === 200) {
-            responseObj.status = 200
-            responseObj.message = "2fa code sent"
-        }
+    if (response.status === 200) {
+      responseObj.status = 200;
+      responseObj.message = 'TWO FACTOR AUTHENTICATION CODE SENT';
     }
-    catch (error) {
-        responseObj.status = 500
-        responseObj.message = "internal server error"
-        responseObj.error = error
-    }
-    return responseObj;
-}
+  } catch (error) {
+    responseObj.status = 500;
+    responseObj.message = 'INTERNAL SERVER ERROR';
+    responseObj.error = error;
+  }
+  return responseObj;
+};
 
 module.exports = send2fa;
