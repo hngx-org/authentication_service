a8a1feef2ba3a866f66ca73f3ef6af1b7c5b4125
Olawole Olawale Martins <olawalemart@gmail.com>
2023-10-16 14:32:55 +0100

    updated 2fa endpoints and added disable2fa

--- controllers/AuthenticationController/verifyUser.js
+++ controllers/AuthenticationController/verifyUser.js
@@ -1,15 +1,13 @@
 const jwt = require('jsonwebtoken');
 const User = require('../../models/Users');
-const {sendWelcomeMail} = require("../MessagingController/sendWelcomeMail");
+const { sendWelcomeMail } = require('../MessagingController/sendWelcomeMail');
 
 const verifyUser = async (req, res, next) => {
   const { token } = req.params;
   const { JWT_SECRET } = process.env;
 
   try {
-
     const decodedUser = jwt.verify(token, JWT_SECRET);
-
 
     // verify user in database
     const user = await User.findOne({ where: { email: decodedUser.email } });
@@ -24,8 +22,8 @@ const verifyUser = async (req, res, next) => {
 
     req.user = user;
     // new response to sign user in immediately after verification
-    const fullName = `${user.first_name} ${user.last_name}`;
-    // sendWelcomeMail(fullName, user.email);
+
+    sendWelcomeMail(fullName, user.email);
     return next();
   } catch (error) {
     next(error);
--- controllers/MessagingController/sendWelcomeMail.js
+++ controllers/MessagingController/sendWelcomeMail.js
@@ -1,21 +1,22 @@
+/*eslint-disable */
 const axios = require('axios');
 async function sendWelcomeMail(name, recipient) {
-    try {
-        const emailServiceUrl = `${process.env.EMAIL_SERVICE_WELCOME_URL}`;
-        const redirectLink = `${process.env.AUTH_FRONTEND_DASHBOARD_URL}`;
+  try {
+    const emailServiceUrl = `${process.env.EMAIL_SERVICE_WELCOME_URL}`;
+    const redirectLink = `${process.env.AUTH_FRONTEND_URL}`;
 
-        const response = await axios.post(emailServiceUrl, {
-            name,
-            recipient,
-            call_to_action: redirectLink,
-        });
-        if (response.status === 200) {
-            console.log('Welcome email sent successfully.');
-        } else {
-            console.error('Failed to send welcome email.');
-        }
-    } catch (error) {
-        console.error('Error sending email:', error.message);
+    const response = await axios.post(emailServiceUrl, {
+      name,
+      recipient,
+      call_to_action: redirectLink,
+    });
+    if (response.status === 200) {
+      console.log('Welcome email sent successfully.');
+    } else {
+      console.error('Failed to send welcome email.');
     }
+  } catch (error) {
+    console.error('Error sending email:', error.message);
+  }
 }
-module.exports = {sendWelcomeMail};
+module.exports = { sendWelcomeMail };
