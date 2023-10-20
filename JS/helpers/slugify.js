b3b8437925197a33448d0fbb0d0c568ce17fd03b
sapphire <chidinmaakunwa@gmail.com>
2023-10-20 19:54:51 +0100

    Fix: Slug Counter

--- JS/helpers/slugify.js
+++ JS/helpers/slugify.js
@@ -1,24 +1,27 @@
 const User = require('../models/Users');
 
-async function slugify(str) {
+async function slugify( firstname, lastname ) {
+  str = firstname + "-" + lastname;
   str = str.replace(/^\s+|\s+$/g, ''); // trim leading/trailing white space
 
   str = str.toLowerCase(); // convert string to lowercase
 
   str = str
     .replace(/[^a-z0-9 -]/g, '') // remove any non-alphanumeric characters
-    .replace(/\s+/g, '-') // replace spaces with hyphens
     .replace(/-+/g, '-'); // remove consecutive hyphens
 
   // check if slug already exists
-  const count = await User.count({ where: { slug: str } });
+  User.count({ where: { first_name: firstname, last_name: lastname } }).then(res => {
+    const count = res;
 
-  // if slug exists, append count to slug
-  if (count) {
-    str += '-' + count;
-  }
+    // if slug exists, append count to slug
+    if (count) {
+      str += '-' + count;
+    }
 
-  return str;
+    return str;
+  })
+
 }
 
 module.exports = slugify;
