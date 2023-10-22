80c85dd1c18e8f0fe0887e7c3d9004a74d224895
Akuya Ekorot <akuyaekorot@gmail.com>
2023-10-22 09:53:03 +0300

    make typescript root folder and remove JS folder

--- JS/middleware/ErrorMiddleware.js
+++
@@ -1,6 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ message: 'Something went wrong!' });
-};
-
-module.exports = { errorHandler };
--- JS/validators/PasswordValidator/index.js
+++
@@ -1,5 +0,0 @@
-const PasswordValidator = {};
-
-PasswordValidator.changePassword = require('./changePassword');
-
-module.exports = PasswordValidator;
